63939f50798ba2e10b0dcdcb4709a22b81493db7
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-23 15:46:58 -0500

    create search function and alert for searching

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      Test
-      <a href="#" className="btn btn-primary shadow">
-        Hello
-      </a>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          {" "}
+          Coded by{" "}
+          <a href="https://ashleyrebeccaxr.com/" targer="_blank">
+            Ashley Goldstein
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/aivxx/Dictionary-App" target="_blank">
+            open-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
