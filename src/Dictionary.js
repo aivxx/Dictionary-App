41d2a5e61549e083addb8158bd9567203444f396
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-23 16:29:35 -0500

    add source

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,6 +15,7 @@ export default function Dictionary() {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
 
+    //documentation https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
