df1c389d613cdf2980bcd97ee9e9e2875e0b44b6
aivxx <90653195+aivxx@users.noreply.github.com>
2022-01-25 19:10:18 -0500

    give image alt text

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -6,7 +6,10 @@ export default function Phonetics(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/2/21/Speaker_Icon.svg"
+          alt="soundIcon"
+        />
       </a>
       <span className="phoneticText">{props.phonetic.text}</span>
     </div>
